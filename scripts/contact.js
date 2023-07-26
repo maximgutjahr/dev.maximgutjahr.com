a947d347e3b01bc4cb6d363dec9ca278b7e60531
bemxio <bemxiov@protonmail.com>
2023-07-26 20:52:57 +0200

    add a contact page

---
+++ scripts/contact.js
@@ -0,0 +1,26 @@
+const button = document.getElementById("contact-form-button");
+
+const nameInput = document.getElementById("contact-form-name");
+const emailInput = document.getElementById("contact-form-email");
+const messageInput = document.getElementById("contact-form-message");
+
+emailjs.init("c80WuKbGmcpaFM0Qr");
+
+button.addEventListener("click", async () => {
+    button.disabled = true;
+    button.innerText = "Bitte warten...";
+
+    const response = await emailjs.send("default_service", "template_lteotsh", {
+        from_name: nameInput.value,
+        reply_to: emailInput.value,
+
+        message: messageInput.value
+
+    });
+
+    if (response.status === 200) {
+        alert("Die Nachricht ging durch! Die Antwort sollte bald verfügbar sein.");
+    } else {
+        alert("Es ist ein Fehler aufgetreten, bitte versuchen Sie es später noch einmal.");
+    }
+});
